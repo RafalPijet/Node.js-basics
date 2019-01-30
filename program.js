a478366690a798877123f2b0523e69dbbe2928b4
Rafal Pijet <stronglopez@wp.pl>
2019-01-30 13:54:49 +0100

    Adding recommendation from 17.3 task

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
 process.stdin.setEncoding("utf-8");
+process.stdout.write("\n\n\nYou can type:\nversion - for node version,\nlanguage - for user's language,\nexit - for exit\n");
 
 process.stdin.on("readable", function () {
     var input = process.stdin.read();
@@ -6,11 +7,24 @@ process.stdin.on("readable", function () {
     if (input != null) {
         var instruction = input.toString().trim();
 
-        if (instruction === "/exit") {
-            process.stdout.write("Quitting app!\n");
-            process.exit();
-        } else {
-            process.stderr.write("Wrong instruction!\n");
+        switch (instruction) {
+            case "version":
+                process.stdout.write("Node version: ");
+                process.stdout.write(process.version +"\n\n\n");
+                break;
+
+            case "language":
+                process.stdout.write("User's language: ")
+                process.stdout.write(process.env.LC_CTYPE +"\n\n\n");
+                break;
+
+            case "exit":
+                process.stdout.write("Quitting app!\n\n\n");
+                process.exit();
+                break;
+
+            default:
+                process.stderr.write("Wrong instruction!\n");
         }
     }
 });
