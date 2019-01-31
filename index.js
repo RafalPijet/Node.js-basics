1211a2a19e5d05b6f47a0eaf97b8df6854783e9e
Rafal Pijet <stronglopez@wp.pl>
2019-01-31 17:38:15 +0100

    Add example 17.7

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 var OSinfo = require("./modules/OSinfo");
 var EventEmitter = require("events").EventEmitter;
+var fs = require("fs");
+var StatMode = require("stat-mode");
 var emitter = new EventEmitter();
 
 process.stdin.setEncoding("utf-8");
+
 emitter.on("beforeCommand", function (instruction) {
     console.log("You wrote: " + instruction + ", trying to run command");
 });
+
 emitter.on("afterCommand", function () {
     console.log("Finished command");
 });
+
+fs.stat("./cat.jpeg", function (err, stats) {
+    var statMode = new StatMode(stats);
+    console.log(statMode.toString());
+});
+
+fs.readFile("./tekst.txt", "utf-8", function (err, data) {
+    console.log("Dane przed zapisem!".blue)
+    console.log(data);
+    fs.appendFile("./tekst.txt", "\nA tak wyglądają po zapisie!", function (err) {
+        if (err) throw err;
+        console.log("Zapisano!".blue);
+        fs.readFile("./tekst.txt", "utf-8", function (err, data) {
+            console.log("Dane po zapisie!".blue);
+            console.log(data);
+        });
+    });
+});
+
 
 process.stdin.on("readable", function () {
    var input = process.stdin.read();
